42a5caaaa5278b264d6af15fd1be5dc16078a743
Dark Patrol <basist031@gmail.com>
2023-01-22 06:12:07 +0300

    make canvs look better

--- myskills.js
+++ myskills.js
@@ -1,19 +1,61 @@
-const ctx = document.getElementById('myskills').getContext('2d');
-let myskills= new Chart(ctx,{
-	type: 'doughnut',
-	data:{
-		labels: ['Html','Css','Js','Php'] ,
-		datasets: [{
-			label:'Мои навыки' ,
-			data: [40,60,30,10] ,
-			backgroundColor: ['red','yellow','blue','green'] ,
-			borderColor:[
-				'black'
-			] ,
-			borderWidth: 4 
-	}],
-	} ,
-	options: {
-		maintainAspectRatio:true,
-	}
+var skillsChart = document.getElementById("myskills");
+
+Chart.defaults.font.family = "Insomnia";
+Chart.defaults.font.size = 22;
+Chart.defaults.color = "black";
+var canvas = document.getElementById('myskills');
+    var context = canvas.getContext('2d');
+
+    // no we get with of content and asign the same hight as the with. this gives up aspect ration 1:1.
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerWidth;
+
+var Skills = {
+  labels: ["Html", "Css", "Js", "Php", "Git"],
+  datasets: [
+    {
+      data: [70, 60, 30, 15, 30],
+      backgroundColor: [
+			'rgba(255, 26, 104, 0.2)',
+			'rgba(54, 162, 235, 0.2)',
+			'rgba(255, 206, 86, 0.2)',
+			'rgba(75, 192, 192, 0.2)',
+			'rgba(153, 102, 255, 0.2)',
+			'rgba(255, 159, 64, 0.2)',
+			'rgba(0, 0, 0, 0.2)'
+			],
+      borderColor: "black",
+      borderWidth: 2
+    }
+  ]
+};
+
+var chartOptions = {
+  rotation: -90,
+  cutout: "45%",
+  plugins: {
+    title: {
+      display: true,
+      position: "bottom",
+			align: "justify",
+      text: "Мои навыки",
+      font: {
+        size: 32
+      }
+    },
+    legend: {
+      position: "left",
+      align: "start"
+    }
+  },
+  animation: {
+    animateRotate: false,
+    animateScale: true
+  }
+};
+
+var donutChart = new Chart(skillsChart, {
+  type: "doughnut",
+  data: Skills,
+  options: chartOptions
 });

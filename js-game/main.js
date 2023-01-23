5dda95fb3253a3d5e03df8c82af490186be82882
Dark Patrol <basist031@gmail.com>
2023-01-23 15:28:23 +0300

    learning how use canvas

---
+++ js-game/main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas');
+const ctx= canvas.getContext('2d')
+
+
+// ctx.strokeStyle='purple'
+// ctx.strokeRect(30,30,500,300);
+// ctx.fillStyle='yellow'
+// ctx.fillRect(100,100,250,150);

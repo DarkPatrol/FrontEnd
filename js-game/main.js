4a96f7cb1114dba800f05964bc15dd08ee39cb4c
Dark Patrol <basist031@gmail.com>
2023-01-23 15:47:22 +0300

    first animation

--- js-game/main.js
+++ js-game/main.js
@@ -1,8 +1,16 @@
 const canvas = document.getElementById('canvas');
 const ctx= canvas.getContext('2d')
 
+let x= 40,dx=4,
+		y=40,dy=4
+ 		
 
-// ctx.strokeStyle='purple'
-// ctx.strokeRect(30,30,500,300);
-// ctx.fillStyle='yellow'
-// ctx.fillRect(100,100,250,150);
+function render(){
+	ctx.clearRect(0,0,canvas.width,canvas.height)
+	x= x+dx;
+	y=y+dy;
+	ctx.fillStyle='purple'
+	ctx.fillRect(x,y,60,60);
+	window.requestAnimationFrame(render)
+}
+window.requestAnimationFrame(render)

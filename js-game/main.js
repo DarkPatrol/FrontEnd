cf9b0164bd9ae81748a718be58bd24d5db1c8286
Dark Patrol <basist031@gmail.com>
2023-01-23 20:34:37 +0300

    vertical ray and ball aded with animation

--- js-game/main.js
+++ js-game/main.js
@@ -1,25 +1,44 @@
-const canvas = document.getElementById('canvas');
+const c = document.getElementById('canvas');
 const ctx= canvas.getContext('2d')
 
-const image= new Image()
+function drawVerticalRay(x, color = 'black') {
+	ctx.beginPath();
+	ctx.moveTo(x, 0);
+	ctx.lineTo(x, c.height);
+	ctx.strokeStyle = color
+	ctx.stroke();
+}
+function drawPoint(x, y, color = 'black') {
+	ctx.beginPath();
+	ctx.ellipse(x, y, 10, 10, Math.PI / 4, 0, 2 * Math.PI);
+	ctx.fillStyle = color
+	ctx.fill();
+}
 
-image.onload= function(){
-	let x= 40,dx=1,
-			y=40,dy=1
- 		
+let xA=0
+let xB=1500
 
 function render(){
-	ctx.clearRect(0,0,canvas.width,canvas.height)
-	x= x+dx;
-	y=y+dy;
+	ctx.clearRect(0,0,c.width,c.height)
+
+	xA= xA+2
+
+	if(xA > xB){
+		drawVerticalRay(xB,'red');
+	}
+	else{
+		drawVerticalRay(xB,'purple');
+	}
+	if(xA>1920){
+		xA=0;
+	}
+
+	drawPoint(xA,540,'black');
 	
-	ctx.drawImage(image,x,y)
 	window.requestAnimationFrame(render)
 }
 window.requestAnimationFrame(render)
-}
 
-image.src='fly.png'
 
 
 

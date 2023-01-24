99a0d4e84342b2696a8ae4a9986fb3706007fbe1
Dark Patrol <basist031@gmail.com>
2023-01-24 18:13:37 +0300

    trying another variant

--- js-game/main.js
+++ js-game/main.js
@@ -1,180 +1,84 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d')
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
-// Draw vertical ray
-function drawVerticalRay(x, color = 'black') {
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
-    ctx.strokeStyle = color
-    ctx.stroke();
-}
-
-// Draw horizontal ray
-function drawHorizontalRay(y, color = 'black') {
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-    ctx.strokeStyle = color
-    ctx.stroke();
-}
-
-// Draw poin
-function drawPoint(point) {
-    ctx.beginPath();
-    ctx.ellipse(point.x, point.y, 10, 10, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = point.color
-    ctx.fill();
-}
-
-// Draw rect
-// rect - объект типа {x: 12, y: 12, width: 21, height: 44, color: 'green'}
-function drawRect(rect) {
-    ctx.beginPath();
-    ctx.rect(rect.x, rect.y, rect.width, rect.height)
-    ctx.fillStyle = rect.color
-    ctx.fill();
-}
-
-// Draw circle
-// circle - объект типа {x: 12, y: 12, radius: 32, color: 'green'}
-function drawCircle(circle) {
-    ctx.beginPath();
-    ctx.ellipse(circle.x, circle.y, circle.radius, circle.radius, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = circle.color
-    ctx.fill();
-}
-
-// Столкновение прямоугольников
-// rect1, rect2 - объекты типа {x: 12, y: 12, width: 21, height: 44, color: 'green'}
-function checkRectsCollision(rect1, rect2) {
-    return rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.height + rect1.y > rect2.y
-}
-
-// Столкновение окружностей
-// circle1, circle2 - объекты типа {x: 12, y: 12, radius: 32, color: 'green'}
-function checkCirclesCollision(circle1, circle2) {
-    const distance = Math.sqrt(Math.pow(circle1.x - circle2.x, 2) + Math.pow(circle1.y - circle2.y, 2))
-
-    return distance < (circle1.radius + circle2.radius)
-}
-
-// Столкновение окружности и прямоугольника
-// circle - объект типа {x: 12, y: 12, radius: 32, color: 'green'}
-// rect - объект типа {x: 12, y: 12, width: 21, height: 44, color: 'green'}
-function checkCircleRectCollision(circle, rect) {
-    let distX = Math.abs(circle.x - rect.x - rect.width / 2);
-    let distY = Math.abs(circle.y - rect.y - rect.height / 2);
-
-    if (distX > (rect.width / 2 + circle.radius)) { return false; }
-    if (distY > (rect.height / 2 + circle.radius)) { return false; }
-
-    if (distX <= (rect.width / 2)) { return true; }
-    if (distY <= (rect.height / 2)) { return true; }
-
-    let dx = distX - rect.width / 2;
-    let dy = distY - rect.height / 2;
-    return (dx * dx + dy * dy <= (circle.radius * circle.radius));
-}
-
-//random numb
-function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-let balls = []
-
-for (let i = 1; i < 10; i++) {
-    balls.push({
-        x: i * 100,
-        y: getRandom(100, 300),
-        dx: getRandom(7, 8),
-        dy: getRandom(7, 8),
-        radius: getRandom(15, 30),
-        color: 'green'
-    })
-}
-
-let rect = {
-    x: 200,
-    y: 400,
-    dx: 3,
-    dy: 3,
-    width: 200,
-    height: 40,
-    color: 'blue'
-}
-
-// events
-const pressed = {}
-canvas.addEventListener('mousemove',function(event){
-	  rect.x = event.offsetX
-		rect.y= event.offsetY
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+canvas.addEventListener('mousemove',function(e){
+	paddleX = e.offsetX
 })
-document.addEventListener('keydown', function(event) {
-    pressed[event.code] = true
-})
-
-document.addEventListener('keyup', function(event) {
-    pressed[event.code] = false
-})
-
-function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (pressed['ArrowRight'] && rect.x + rect.width <= canvas.width) {
-        rect.x = rect.x + rect.dx
-    }
-
-    if (pressed['ArrowLeft'] && rect.x >= 0) {
-        rect.x = rect.x - rect.dx
-    }
-
-    if (pressed['ArrowUp'] && rect.y >= 0) {
-        rect.y = rect.y - rect.dy
-    }
-
-    if (pressed['ArrowDown'] && rect.y + rect.height <= canvas.height) {
-        rect.y = rect.y + rect.dy
-    }
-
-
-    // motion
-    for (const ball of balls) {
-        ball.x = ball.x + ball.dx
-        ball.y = ball.y + ball.dy
-    }
-
-    // Коллиззии
-    for (const ball of balls) {
-
-        if (ball.y + ball.radius >= canvas.height) {
-            ball.color = "red"
-        }
-
-        if ((ball.y + ball.radius >= canvas.height) || (ball.y - ball.radius <= 0) || checkCircleRectCollision(ball, rect)) {
-            ball.dy = ball.dy * (-1)
-            ball.y = ball.y + ball.dy
-        }
-
-        if ((ball.x + ball.radius >= canvas.width) || (ball.x - ball.radius <= 0) || checkCircleRectCollision(ball, rect)) {
-            ball.dx = ball.dx * (-1)
-            ball.x = ball.x + ball.dx
-        }
-
-
-    }
-
-    //render
-    for (const ball of balls) {
-        drawCircle(ball)
-    }
-    drawRect(rect)
-
-    window.requestAnimationFrame(render)
+function keyDownHandler(e) {
+		if(e.code  == "ArrowRight") {
+				rightPressed = true;
+		}
+		else if(e.code == 'ArrowLeft') {
+				leftPressed = true;
+		}
+}
+function keyUpHandler(e) {
+		if(e.code  == "ArrowRight") {
+				rightPressed = false;
+		}
+		else if(e.code == 'ArrowLeft') {
+				leftPressed = false;
+		}
 }
 
-window.requestAnimationFrame(render)
+function drawBall() {
+		ctx.beginPath();
+		ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+		ctx.fillStyle = "#0095DD";
+		ctx.fill();
+		ctx.closePath();
+}
+function drawPaddle() {
+		ctx.beginPath();
+		ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+		ctx.fillStyle = "#0095DD";
+		ctx.fill();
+		ctx.closePath();
+}
+
+function draw() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawBall();
+		drawPaddle();
+
+		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+				dx = -dx;
+		}
+		if(y + dy < ballRadius) {
+				dy = -dy;
+		}
+		else if(y + dy > canvas.height-ballRadius) {
+				if(x > paddleX && x < paddleX + paddleWidth) {
+						dy = -dy;
+				}
+				else {
+						clearInterval(game);
+						console.log("GAME OVER");
+						document.location.reload();
+				}
+		}
+
+		if(rightPressed && paddleX < canvas.width-paddleWidth) {
+				paddleX += 7;
+		}
+		else if(leftPressed && paddleX > 0) {
+				paddleX -= 7;
+		}
+
+		x += dx;
+		y += dy;
+}
+
+var game = setInterval(draw, 10);
